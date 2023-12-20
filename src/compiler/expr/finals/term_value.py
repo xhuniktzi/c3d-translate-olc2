35ea85d83d6fd67677d98446c612ed13cc0a17f5
Xhunik Miguel <xhuniktzi@gmail.com>
2023-12-19 22:44:56 -0600

    Revert "hola"
    This reverts commit c5d00c148076bf8034679dd75fcdb05807d0ef9c.

--- src/compiler/expr/finals/term_value.py
+++ src/compiler/expr/finals/term_value.py
@@ -11,4 +11,32 @@ class TermValue(Expression):
         self.datatype: DataTypes = datatype
 
     def translate_to_c3d(self, env: Environment) -> C3DValue:
-        pass
+        if self.datatype == DataTypes.ENTERO:
+            return C3DValue(f"{self.value}.0", False, self.datatype)
+        elif self.datatype == DataTypes.DECIMAL:
+            return C3DValue(f"{self.value}", False, self.datatype)
+        elif self.datatype == DataTypes.IDVARIABLE:
+            return C3DValue(f"{self.value}", False, self.datatype)
+        elif self.datatype == DataTypes.CADENA:
+            temp_var: str = self.generator.mk_temp()
+            self.generator.simple_assign(
+                temp_var, "h"
+            )  # temp_var = h; save the position of the heap
+            for char in self.value:
+                self.generator.register_write_heap("h", f"{ord(char)}")
+                self.generator.register_next_heap()
+
+            self.generator.register_write_heap("h", "-1")  # end of string
+            self.generator.register_next_heap()
+
+            return C3DValue(temp_var, True, self.datatype)
+
+        # elif self.datatype == DataTypes.CADENA:
+        #     temp_var: str = self.generator.mk_temp()
+        #     self.generator.register_c3d_expression(temp_var, "h", "", "")
+
+        #     for char in self.value:
+        #         self.generator.register_write_heap("h", f"{ord(char)}")
+        #         self.generator.register_next_heap()
+
+        #     return C3DValue(temp_var, True, self.datatype)
